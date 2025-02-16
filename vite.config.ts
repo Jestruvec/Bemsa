c6a92faef7635acbd2f7a8ddb4cde1d0e566f0ad
jestruvec <jhonnyestruve@gmail.com>
2025-02-16 10:20:27 -0600

    se agrega base en vite para solucionar error 404 en github pages

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import tailwindcss from "@tailwindcss/vite";
 import path from "path-browserify";
 
 export default defineConfig({
+  base: "/Bemsa/",
   plugins: [vue(), tailwindcss()],
   resolve: {
     alias: {
