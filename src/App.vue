992e244905be5f72d77511c18c987371b3789c4c
jestruvec <jhonnyestruve@gmail.com>
2025-02-26 19:31:59 -0600

    secrets

--- src/App.vue
+++ src/App.vue
@@ -5,11 +5,5 @@
 </template>
 
 <script lang="ts" setup>
-import { onMounted } from "vue";
 import { RouterView } from "vue-router";
-
-onMounted(() => {
-  console.log("Supabase URL:", import.meta.env.VITE_SUPABASE_URL);
-  console.log("Supabase Key:", import.meta.env.VITE_SUPABASE_ANON_KEY);
-});
 </script>
